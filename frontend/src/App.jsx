4ee5e0a4695e7c5e5b4913f840f038db2a862362
Duy Long <wincute4321@gmail.com>
2026-06-03 19:22:19 +0700

    Improve UI 2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,7 +24,7 @@ function App() {
 
       <main className="container">
         <section className="hero">
-          <span className="badge">🚀 DevOps Final Project</span>
+          <span className="badge">🚀 DevOps Final Project v2</span>
 
           <h1>
             React + Node.js + PostgreSQL
